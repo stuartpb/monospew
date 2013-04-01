8c27aad21abaef76b183592a77bd24c6e54c4c55
Stuart P. Bentley <stuart@testtrack4.com>
2013-04-01 02:29:56 -0400

    Fix blank characters at end of old lines when resizing
    The old behavior is not attainable (I didn't feel it was desirable enough to throw conditions into fitLines).
    The next commit will probably remove opts.instant optionality altogether.
    
    Contains tentative, non-working left and right append logic. Committing now because it's going to involve a lot of refactoring.

--- monospew.js
+++ monospew.js
@@ -1,4 +1,6 @@
 function monospew(element,opts){
+  "use strict";
+
   if(typeof element == "string"){
     element = document.querySelector(element);
   }
@@ -8,24 +10,40 @@ function monospew(element,opts){
   var height = opts.height || 24;
 
   function cullExtraLines(content) {
-    while((content.match(/\n/g)||[]).length >= height) {
-      if(opts.append == 'top') {
-        content = content.replace(/[^\n]*\n$/,'');
-      } else {
-        content = content.replace(/[^\n]*\n/,'');
-      }
+    var lines = content.split('\n');
+    if(opts.append == 'top') {
+      return lines.slice(0,Math.min(lines.length+1, height+1)).join('\n');
+    } else {
+      return lines.slice(Math.max(lines.length-1-height, 0)).join('\n');
     }
-    return content;
   }
 
-  function randomString(num){
+  function fitLines(content) {
+    if(opts.append == 'left') {
+      return content.replace(/^.*$/mg,function(line){
+        return randomString(Math.max(width-line.length,0))
+          + line.slice(-width);
+      });
+    } else {
+      return content.replace(/^.*$/mg,function(line){
+        return line.slice(0,width) +
+          randomString(Math.max(width-line.length,0));
+      });
+    }
+  }
+
+  function randomChar() {
     var chars = opts.chars ||
       ' !"#$%&\'()*+,-./0123456789:;<=>?' +
       '@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_' +
       '`abcdefghijklmnopqrstuvwxyz{|}~';
+    return chars[Math.floor(Math.random()*chars.length)];
+  }
+
+  function randomString(num){
     var lineChars = [];
     for(var i = 0; i < num; i++) {
-      lineChars[i]=chars[Math.floor(Math.random()*chars.length)];
+      lineChars[i]=randomChar();
     }
     return lineChars.join('');
   }
@@ -34,23 +52,35 @@ function monospew(element,opts){
     return randomString(width) + '\n';
   }
 
-  function addLine(base) {
-    if(opts.append == 'top') {
+  function append(base) {
+    if(opts.append == 'left') {
+      return base.replace(/^.*$/mg,function(line){return randomChar() + line});
+    } else if(opts.append == 'right') {
+      return base.replace(/^.*$/mg,function(line){return line + randomChar()});
+    } else if(opts.append == 'top') {
       return randomLine() + base;
     } else {
       return base + randomLine();
     }
   }
 
-  retobj.resize = function(w,h) {
-    width = w || width; height = h || height;
+  function instafill() {
     if(opts.instant !== false){
-      var newContent = cullExtraLines(element.textContent);
+      var newContent = fitLines(cullExtraLines(element.textContent));
       while((newContent.match(/\n/g)||[]).length < height) {
-        newContent = addLine(newContent);
+        if(opts.append == 'top') {
+          newContent = randomLine() + newContent;
+        } else {
+          newContent += randomLine();
+        }
       }
       element.textContent = newContent;
     }
+  }
+
+  retobj.resize = function(w,h) {
+    width = w || width; height = h || height;
+    instafill();
   };
 
   retobj.listener = function() {
@@ -86,14 +116,17 @@ function monospew(element,opts){
       }
       var bw = fillel.clientWidth;
       var bh = fillel.clientHeight;
-      retobj.resize(Math.floor(bw/cw) + (opts.bottomExtra || 0),
-        Math.floor(bh/ch) + (opts.rightExtra || 0));
+      retobj.resize(Math.floor(bw/cw) + (opts.extraHeight || 0),
+        Math.floor(bh/ch) + (opts.extraWidth || 0));
+    } else {
+      //if not filling a particular element, still check if we should
+      //fill the area we're set for
+      instafill();
     }
   };
 
   retobj.timer = function() {
-    element.textContent = addLine(
-      cullExtraLines(element.textContent));
+    element.textContent = fitLines(cullExtraLines(append(element.textContent)));
   };
 
   //The interval / listener that has been configured for this function.
